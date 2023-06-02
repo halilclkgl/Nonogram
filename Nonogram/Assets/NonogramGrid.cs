5e1fd4c0e518374aa56214ce3d681616234d810b
halilclkgl <119314365+halilclkgl@users.noreply.github.com>
2023-06-02 20:02:49 +0300

    v1
    v1

---
+++ Nonogram/Assets/NonogramGrid.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NonogramGrid : MonoBehaviour
+{
+    [SerializeField] private GameObject[] cellObjects;
+    [SerializeField] private TMP_Text [] hintTexts;
+    [SerializeField] private TMP_Text[] hintTexts2;
+    private bool[,] cellValues;
+    private bool[,] solutionArray;
+    int gridSize = 4;
+    private int sayac = 0;
+    private string ipucu;
+    private void Start()
+    {
+        solutionArray = GenerateRandomSolution();
+        // Hücre deðerleri için 2D array'i oluþturun
+        cellValues = new bool[gridSize, gridSize];
+
+        // Hücre objelerini array'e atayýn
+        cellObjects = new GameObject[gridSize * gridSize];
+
+        for (int row = 0; row < gridSize; row++)
+        {
+            List<int> hintValues = new List<int>(); // Ýpucu deðerlerini tutacak liste
+            int consecutiveTrueCount = 0; // Ardýþýk true sayýsýný tutacak deðiþken
+
+            for (int col = 0; col < gridSize; col++)
+            {
+                string cellName = "(" + row + "," + col + ")";
+                cellObjects[row * gridSize + col] = GameObject.Find(cellName);
+
+                if (solutionArray[row, col] == true)
+                {
+                    consecutiveTrueCount++;
+                }
+                else
+                {
+                    if (consecutiveTrueCount > 0)
+                    {
+                        hintValues.Add(consecutiveTrueCount);
+                        consecutiveTrueCount = 0;
+                    }
+                }
+            }
+
+            if (consecutiveTrueCount > 0)
+            {
+                hintValues.Add(consecutiveTrueCount);
+            }
+
+            // Ýpucu deðerlerini text kutusuna aktar
+            string hintString = string.Join(" ", hintValues.ToArray());
+            hintTexts[row].text = hintString;
+            
+        }
+        for (int row = 0; row < gridSize; row++)
+        {
+            List<int> hintValues = new List<int>(); // Ýpucu deðerlerini tutacak liste
+            int consecutiveTrueCount = 0; // Ardýþýk true sayýsýný tutacak deðiþken
+
+            for (int col = 0; col < gridSize; col++)
+            {
+                if (solutionArray[col, row] == true)
+                {
+                    consecutiveTrueCount++;
+                }
+                else
+                {
+                    if (consecutiveTrueCount > 0)
+                    {
+                        hintValues.Add(consecutiveTrueCount);
+                        consecutiveTrueCount = 0;
+                    }
+                }
+            }
+
+            if (consecutiveTrueCount > 0)
+            {
+                hintValues.Add(consecutiveTrueCount);
+            }
+
+            string hintString = string.Join(" ", hintValues.ToArray()); // Ýpucu deðerlerini birleþtirerek string haline getirme
+            hintTexts2[row].text = hintString;
+        }
+
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Týklanan pozisyonu dünya koordinatlarýndan grid içindeki hücre konumuna çevir
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            int row = Mathf.FloorToInt(mousePosition.x);
+            int col = Mathf.FloorToInt(mousePosition.y);
+
+            // Hücrenin durumunu deðiþtir
+           // ToggleCell(row, col);
+            UpdateCellVisuals();
+           
+        }
+    }
+    private bool[,] GenerateRandomSolution()
+    {
+        bool[,] array = new bool[gridSize, gridSize];
+        for (int row = 0; row < gridSize; row++)
+        {
+            for (int col = 0; col < gridSize; col++)
+            {
+                array[row, col] = Random.value < 0.5f; // %50 olasýlýkla true veya false
+            }
+        }
+        return array;
+    }
+  
+    private void UpdateCellVisuals()
+    {
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        int row = Mathf.FloorToInt(mousePosition.x);
+        int col = Mathf.FloorToInt(mousePosition.y);
+
+        bool isSolutionCellTrue = solutionArray[row, col];
+        bool isClickedCellTrue = cellValues[row, col];
+
+        GameObject cellObject = cellObjects[row * gridSize + col];
+        SpriteRenderer spriteRenderer = cellObject.GetComponent<SpriteRenderer>();
+       
+        Debug.Log(isSolutionCellTrue);
+        Debug.Log(isClickedCellTrue);
+        if (isSolutionCellTrue)
+        {
+            solutionArray[row, col] = true;
+            spriteRenderer.color = Color.black;
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
+
+    }
+}
